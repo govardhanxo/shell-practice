29a465a2c97727556ce793ff1d025dd54dbd6269
govardhan <govardhanxo@gmail.com>
2025-05-27 15:27:26 +0530

    data types started

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -3,10 +3,10 @@
 NUMBER1=100
 NUMBER2=200
 
-$TIMESTAMP=$(date)
+TIMESTAMP=$(date)
 
 echo "script executed at: $TIMESTAMP"
-$SUM=$(($NUMBER1+$NUMBER2))
+SUM=$(($NUMBER1+$NUMBER2))
 
 echo "SUM of $NUMBER1 and $NUMBER2 is: $SUM"
 
