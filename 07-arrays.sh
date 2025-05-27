293b4220e55586a3d18ba622a84ffebdb62f8252
govardhan <govardhanxo@gmail.com>
2025-05-27 15:59:24 +0530

    arrays started

---
+++ 07-arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+Movies=("COURT" "HIT3" "PUSHPA2" "THANDEL")
+
+echo "First movie is: ${Movies[0]}"
+echo "Second movie is: ${Movies[1]}"
+echo "Third movie is: ${Movies[2]}"
+echo "Fourth movie is: ${Movies[3]}"
+
+echo "All movies are: ${Movies[@]}"
+
