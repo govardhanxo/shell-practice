d3d53fd136d8fd2139aab15626ff57e3c9285b21
govardhan <govardhanxo@gmail.com>
2025-05-27 13:19:46 +0530

    conversation started

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -2,7 +2,7 @@
 
 echo "Trump:: Hey Musk, How are you?"
 echo "Musk:: Hello Trump, I am fine. How are you doing"
-echo "Trump: I am fine too. What's up?"
+echo "Trump:: I am fine too. What's up?"
 echo "Musk:: Nohting, just going to Mars now, will you come?"
 echo "Trump:: Sorry, you carry on! I will come once you come back"
 
