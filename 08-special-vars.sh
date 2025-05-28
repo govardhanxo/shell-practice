bd4239346bf431bb8c6890e075ef2b02dea43e4e
govardhan <govardhanxo@gmail.com>
2025-05-28 10:33:57 +0530

    special vars

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 echo "All variables passed to the script: $@"
-
 echo "Number of variables: $#"
+echo "Script name: $0"
+echo "Current directory: $PWD"
+echo "User running this script: $USER"
